071e040f9bd2e172586e3f5d920d2414514a4538
nseyoum <nseyoum@ucsd.edu>
2022-02-04 00:04:37 -0800

    fixed failed test

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class SkillDemoTest{
     @Test
     public void multiplication(){
-        assertEquals(3,SkillDemo.multiply(2,1)); //fails at first
+        assertEquals(2,SkillDemo.multiply(2,1)); //fails at first
     }
 }
